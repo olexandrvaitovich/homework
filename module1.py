c8dcb534d4d1f63fddc9f7e6d468b5a52089d843
ostapViniavskyi <viniavskyi@ucu.edu.ua>
2018-01-31 13:07:01 +0200

    completed module 1

--- module1.py
+++ module1.py
@@ -2,12 +2,20 @@ import math
 
 
 def data_io():
-    """console data input and result output"""
-    a = float(input('a = '))
-    b = float(input('b = '))
-    c = float(input('c = '))
-    e = float(input('epsilon = '))
-    # check what two sides are equal
+    """Сonsole data input and result output"""
+    # data input
+    while True:
+        try:
+            print('Type lenght of each side:')
+            a = float(input('a = '))
+            b = float(input('b = '))
+            c = float(input('c = '))
+            e = float(input('epsilon = '))
+            break
+        except ValueError:
+            print('Wrong input!')
+
+    # check which two sides are equal
     if a == b:
         ans = find_side(a, b, c, e)
     elif b == c:
@@ -15,27 +23,34 @@ def data_io():
     elif a == c:
         ans = find_side(c, a, b, e)
     else:
-        print('wrong input')
-    print(ans)
+        print('Triangle has no equal sides!')
+    print("x = ", ans)
 
 
 def find_side(a, b, c, e):
     """
+    (number, number, number, number) -> number
     find length of the side of square inscribed in the triangle
+
+    >>> find_side(10, 10, 12, 0.01)
+    4.80029296875
+    >>> find_side(12, 12, 20, 0.01)
+    4.98046875
     """
     # fing the height of triangle
     h = math.sqrt(a**2 - (c / 2)**2)
-    left = 0
-    right = c
+    left, right = 0, c
     x = (left + right) / 2
+    total_area = c * h / 2 - x * (c - x) / 2 - (h - x) * x / 2 - x * x
     # using binary search find the answer
-    while abs(c * h / 2 - x * (c - x) / 2 - (h - x) * x / 2 - x * x) > e:
-        if c * h / 2 - x * (c - x) / 2 - (h - x) * x / 2 - x * x > 0:
+    while abs(total_area) > e:
+        if total_area > 0:
             left = x
         else:
             right = x
         x = (left + right) / 2
-    return int(x / e) * e
+        total_area = c * h / 2 - x * (c - x) / 2 - (h - x) * x / 2 - x * x
+    return x
 
 
 if __name__ == '__main__':
